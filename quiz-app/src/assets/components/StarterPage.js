e7d50fe788537b80226b04aac395cd69e300b85a
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-11 09:37:03 -0500

    I splitted the first page into its separate compeont and imported into App to be able to have ternary and display either welcome page or the playing part. Otherwise I was getting an empty div on the top before the quiz content

---
+++ quiz-app/src/assets/components/StarterPage.js
@@ -0,0 +1,13 @@
+function StarterPage(props) {
+  
+    return (
+        <div id="first-page-container">
+            <h1 id="intro-title">Lets start our Quiz</h1>
+            <p id="instructions">There will be 5 questions and you need to choose the right answers and submit and check. 
+            You can play as many times as you want. Click "Start Quiz" to start!!!</p>
+            <button id="startGameButton" onClick={props.SetStartGame}>Start Quiz</button>
+        </div>
+    );
+  }
+  
+  export default StarterPage;
