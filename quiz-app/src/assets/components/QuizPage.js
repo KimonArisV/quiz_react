26d8e1b500d9f62211f95c9a891e8e0502e82e4f
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-12 06:42:10 -0500

    adding comments into the QuizPage

--- quiz-app/src/assets/components/QuizPage.js
+++ quiz-app/src/assets/components/QuizPage.js
@@ -4,44 +4,42 @@ import {nanoid} from "nanoid";
 
 export default function QuizPage(props) {
 
-    //function for the creation of components fo answers:
-    // const firstdata = fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-    //             .then( response => response.json())
-    //             .then( data => data.results);
-    // const createQuestionComponent = (_,id) => <QuestionBolck key={nanoid()} id={id} data={firstdata[id]} /> ;
-    // const CreateQuestions = () => Array(5).fill().map(createQuestionComponent);
-    // const CreateQuestions1 = ()=>{
-    //         fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-    //         .then( response => response.json()) 
-    //         .then( data => setquestionList( data ))
-    //     };
+    //state hook for the list of questions we will get from fetching the api
     const [questionList, setquestionList] = useState([]);
+    // needed to know when to refetch and when we already show the answers
     const [isAnswersVisible, setisAnswersVisible] = useState(false);
+    //needed to keep track of the players score
     const [score, setScore]=useState(0);
-    //console.log(questionList);
+
+    //making sure it only fectes data when we start a new game
     useEffect(
         ()=>{
             if(!isAnswersVisible){
-                //const newdata = 
+                //fecting the data from the API
                 fetch("https://opentdb.com/api.php?amount=5&type=multiple")
                 .then( response => response.json())
-                .then( data => setquestionList(()=>Array(5).fill().map((_,id) => <QuestionBlock key={nanoid()} id={id} data={data.results[id]} /> )));
+                .then( data => setquestionList(()=>Array(5).fill().map((_,id) => 
+                    <QuestionBlock 
+                        key={nanoid()} 
+                        id={id} 
+                        data={data.results[id]}
+                    /> )));
+                //reseting score in every new game
                 setScore(0);
             };
             },
         [props.startGame,isAnswersVisible]
         );
-    // useEffect(
-    //     ()=>{},
-    //     [questionList]
-    // );
 
     return(
         <div id="game-container">
+            //these will be the questions and choises
             {questionList}
+            //this will be the button to check and restart
             <button className="CheckAnswerButton" onClick={()=>setisAnswersVisible(prevState=>!prevState)}>
                 {!isAnswersVisible ? "Check Answers" : "Play Again"}
             </button>
+            //this will show your score every time
             {isAnswersVisible && <p>You scored {score}/5</p>}
         </div>
     )
